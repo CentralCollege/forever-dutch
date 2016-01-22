e77efe2f14296f7ceb928a480587e52ad0dc090e
Jacob Oyen <oyenj@central.edu>
2016-01-22 17:37:35 -0600

    Remove Emjois

--- functions.php
+++ functions.php
@@ -22,6 +22,18 @@
 		remove_meta_box( 'dashboard_recent_drafts', 'dashboard', 'side');
 	}
 
+// Remove Emoji stuff
+function CUI_disable_emojis() {
+	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+	remove_action( 'wp_print_styles', 'print_emoji_styles' );
+	remove_action( 'admin_print_styles', 'print_emoji_styles' );
+	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+}
+add_action( 'init', 'CUI_disable_emojis' );
+
 /* Add additional thumbnail sizes*/
 	add_theme_support( 'post-thumbnails' );
 	add_image_size( 'central-home-large', 400, 266, true);
