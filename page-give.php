4c56b804a140892651681d828d18679f1211b5d5
Jacob Oyen <oyenj@central.edu>
2016-01-27 16:41:05 -0600

    Add online giving page.

---
+++ page-give.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+    <section class="red keystone">
+      <div class="container">
+        <h1>Your Time is now.</h1>
+        <h2>Make your gift.</h2>
+      </div>
+    </section>
+    <section class="give">
+      <div class="container">
+        <iframe src="https://gateway.sourceallies.com/central/alumni/" width="100%" height="1300px" frameborder="0" scrolling="auto"></iframe>
+      </div>
+    </section>
+    <?php get_footer();?>
