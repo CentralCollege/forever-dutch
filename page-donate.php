077009d417878625892dd5b83bb5bf1bb54dbae1
Jacob Oyen <oyenj@central.edu>
2016-01-27 10:45:20 -0600

    Initial Commit

---
+++ page-donate.php
@@ -0,0 +1,102 @@
+<?php get_header(); ?>
+    <section class="red keystone">
+      <div class="container">
+        <h1>Your Time.</h1>
+        <h2>A financial investment. An enduring legacy.</h2>
+        <p>Those who have come before us helped create the Central legacy. Now, at this defining moment, it falls to this generation to carry it forward. The need is great. The time is now. Your investment helps reaffirm our pledge to further the intellectual, physical and spiritual development of future Central students.</p>
+      </div>
+    </section>
+
+    <section class="videoPromo">
+      <div class="container">
+        <div class="four columns videoTitle">
+          <h2 class="redText">Time for a<br />Transformation.</h2>
+        </div>
+        <div class="eight columns">
+          <img src="//192.168.56.111/wp-content/themes/foreverDutch/images/givingLevelsChart.svg" class="Giving Levels" class="u-max-full-width">
+        </div>
+        <div class="clearBoth"></div>
+      </div>
+    </section>
+    <section class="lightGray dropTop">
+      <div class="breathe container">
+        <h2 class="whiteText">Giving Opportunities</h2>
+        <table class="tableWithStyle">
+          <tr>
+            <th>Naming Levels</th>
+            <th>Gift Amount Required</th>
+            <th>Room/Space Example</th>
+            <th>Room/Spaces Available</th>
+          <tr>
+          <tr>
+            <td>Hall of Fame</td>
+            <td>$1 Million +</td>
+            <td>Upper Lobby/Atrium</td>
+            <td>3</td>
+          </tr>
+          <tr>
+            <td>National Champ</td>
+            <td>$550k - $999k</td>
+            <td>All American Corridor</td>
+            <td>4</td>
+          </tr>
+          <tr>
+            <td>All American</td>
+            <td>$250k - $499k</td>
+            <td>Women's Locker Room</td>
+            <td>4</td>
+          </tr>
+          <tr>
+            <td>Conference Champ</td>
+            <td>110k - 249k</td>
+            <td>Coach's Office Suite</td>
+            <td>11</td>
+          </tr>
+          <tr>
+            <td>Game-Winner</td>
+            <td>$50k - $99k</td>
+            <td>Athletic Director's Office</td>
+            <td>18</td>
+          </tr>
+          <tr>
+            <td>Home Run</td>
+            <td>$25k - 49k</td>
+            <td>Assistant Coach's Office</td>
+            <td>11</td>
+          </tr>
+        </table>
+      </div>
+    </section>
+    <section class="coachSkipQuote">
+      <div class="top container">
+        <blockquote>If I can light a fire under one kid, then it's all worth it." <cite>Coach Ron Schipper</cite></blockquote>
+        <p><a href="/donate" class="redButton pullRight">Donate Now</a></p>
+      </div>
+    </section>
+    <section class="red">
+      <div class="breathe container">
+        <div class="three columns">
+          <img src="//192.168.56.111/wp-content/themes/foreverDutch/images/94percent.svg" alt="94 percent" class="u-max-full-width">
+        </div>
+        <div class="eight columns">
+          <h3>The A.N. Kuyper Athletics Complex serves more than athletes. <strong>More than 94 percent</strong> of Central’s students were athletics facilities users in the past year. </h3>
+        </div>
+        <div class="clearBoth"></div>
+      </div>
+    </section>
+    <img src="//192.168.56.111/wp-content/themes/foreverDutch/images/kuyperHallway.jpg" alt="Schipper Stadium" class="u-max-full-width">
+    <section class="darkGray">
+      <div class="breathe container">
+        <h2 class="redText">Out of Room.</h2>
+        <p><strong>P.H. Kuyper Gymnasium’s aging infrastructure must be replaced.</strong> And a building designed to serve 200 athletes in 1969, now must handle more than 700, turning hallways into athletic training treatment areas. Facilities are in use from 5 a.m.-11 p.m.</p>
+        <p>It’s often been said that the athletics department is a college’s front porch. Student-athletes comprise a significant part of Central’s enrollment and many of the college’s competitors are making major athletics facilities upgrades.</p>
+        <p><strong>This is the time not just for a renovation, but a transformation that will accentuate Central’s position as an intercollegiate leader, as well as a model for whole-person development.</strong></p>
+      </div>
+    </section>
+<img src="//192.168.56.111/wp-content/themes/foreverDutch/images/stadium.jpg" alt="Schipper Stadium" class="u-max-full-width">
+    <section class="quoteSpace">
+      <div class="container">
+        <blockquote>Football made me set standards for myself as an athlete, as well as a student—to do everything with a passion and a purpose, and to do it to my full capability. There is no doubt in my mind that playing  football at Central College changed my life, and it changed it for the better." <cite>Travis Hook '13</cite></blockquote>
+      </div>
+    </section>
+    <?php get_footer();?>
