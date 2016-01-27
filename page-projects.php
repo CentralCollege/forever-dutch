9399a99f6ecd705f39e386201ba767c566d5b4ce
Jacob Oyen <oyenj@central.edu>
2016-01-27 10:45:40 -0600

    Initial Commit

---
+++ page-projects.php
@@ -0,0 +1,81 @@
+<?php get_header(); ?>
+    <section class="red keystone">
+      <div class="container">
+        <h1>A Transformation.</h1>
+        <h2>Championship facilities for an elite program.</h2>
+        <p>The transformation of the A.N. Kuyper Athletics Complex has begun and we need your help to see it through. Forever Dutch began with outdoor field enhancements. Next is expansion of P.H. Kuyper Gymnasium, followed by the building’s renovation and possible additional construction.</p>
+      </div>
+    </section>
+    <section class="phase">
+      <div class="container breathe">
+        <h2>Phase 1: <span class="grayText">Complete</span></h2>
+        <h4 class="redText">Outdoor field upgrades</h4>
+        <ul class="list-group">
+          <li class="list-group-item"><i class="fa fa-check-square fa-fw"></i>New Baseball Dugouts, Stadium Seating</li>
+          <li class="list-group-item"><i class="fa fa-check-square fa-fw"></i>New Softball Stadium Seating</li>
+          <li class="list-group-item"><i class="fa fa-check-square fa-fw"></i>New Soccer Hillside Stadium Seating</li>
+          <li class="list-group-item"><i class="fa fa-check-square fa-fw"></i>Ron and Joyce Schipper Stadium improvements, including Heritage Plaza</li>
+          <li class="list-group-item"><i class="fa fa-check-square fa-fw"></i>PPI Videoboard</li>
+        </ul>
+      </div>
+    </section>
+    <section class="photo red">
+      <div class="container">
+        <h2>Photos will go here...</h2>
+      </div>
+    </section>
+    <section class="phase">
+      <div class="container breathe">
+        <div class="one-half column">
+          <h2>Phase 2: <span class="grayText">Fundraising</span></h2>
+          <h4 class="redText">P.H. Kupyer Gymansium Expansion</h4>
+          <ul class="list-group">
+            <li class="list-group-item"><i class="fa fa-square-o fa-fw"></i>New P.H. Kupyer Gymansium Entrance</li>
+            <li class="list-group-item"><i class="fa fa-square-o fa-fw"></i>New Athletics Hall of Honor</li>
+            <li class="list-group-item"><i class="fa fa-square-o fa-fw"></i>New Lower-Level Lobby</li>
+            <li class="list-group-item"><i class="fa fa-square-o fa-fw"></i>New Upper-Level Lobby</li>
+            <li class="list-group-item"><i class="fa fa-square-o fa-fw"></i>All-American Corridor</li>
+            <li class="list-group-item"><i class="fa fa-square-o fa-fw"></i>new Varsity Locker Room</li>
+            <li class="list-group-item"><i class="fa fa-square-o fa-fw"></i>New Wrestling Room</li>
+            <li class="list-group-item"><i class="fa fa-square-o fa-fw"></i>Major Infrastructure Improvements</li>
+          </ul>
+        </div>
+        <div class="one-half column breathe">
+          <p class="redText"><strong>Anticipated future phases may include</strong>new recruitment and office space, a anew team eeting room, an expanded athletic training room and practice/competition facilities.</p>
+        </div>
+        <div class="clearBoth"></div>
+    </section>
+    <section class="photo red">
+      <div class="container">
+        <h2>Photos will go here...</h2>
+      </div>
+    </section>
+    <section class="fundraisingTotal breathe">
+      <div class="container">
+        <div class="four columns">
+          <h2>Total Phase 1 and 2 costs exceed $15 million.</h2>
+        </div>
+        <div class="eight columns">
+          <img src="//192.168.56.111/wp-content/themes/foreverDutch/images/fundraisingTotal.svg" alt="Fundraising Total" class="u-max-full-width">
+        </div>
+        <div class="clearBoth"></div>
+      </div>
+    </section>
+    <section class="darkGray breathe">
+      <div class="container">
+        <div class="four columns">
+          <img src="//192.168.56.111/wp-content/themes/foreverDutch/images/jersey.svg" alt="Fundraising Total" class="u-max-full-width">
+        </div>
+        <div class="eight columns">
+          <h2 class="redText">We need you on our team.</h2>
+          <p>When everyone contributes, our team grows stronger. This is your team. Now is your time. Your investment will help ensure Central’s continued ability to serve others.</p>
+        </div>
+        <div class="clearBoth"></div>
+      </div>
+    </section>
+    <section class="quoteSpace">
+      <div class="container">
+        <blockquote>I think we have something special happening here. This is our time to add something extra to this rich tradition.”<cite>Riley Gray '17</cite></blockquote>
+      </div>
+    </section>
+    <?php get_footer();?>
