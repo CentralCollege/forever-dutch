97ef50f1e1c970a268d33ee8bd4e040c050fc331
Jacob Oyen <oyenj@central.edu>
2016-01-27 16:38:12 -0600

    News and Events formatting with appropriate headings.

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php get_header();?>
+<section class="red keystone">
+	<div class="container">
+		<?php if(in_category('News')){?><h1>News</h1><?php }
+		else if (in_category('Events')){?><h1>Events</h1><?php } ?>
+		<h2>Stay connected.</h2>
+	</div>
+</section>
 <?php if (have_posts()) : while (have_posts()) : the_post();?>
-	<h1><?php the_title();?></h1>
-	<?php the_content(); ?>
+	<div class="container breathe newsEvents">
+		<div class="post">
+			<div class="meta"><?php echo the_category(' '); ?> // <?php the_time( get_option( 'date_format' ) ) ?></div>
+			<h2 class="redText"><?php the_title();?></h2>
+			<?php the_content(); ?>
+		</div>
+	</div>
 	<?php endwhile; ?>
 	<?php else : ?>
 		<div class="container">
