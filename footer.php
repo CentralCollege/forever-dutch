6eff3ef1913e1759834fdb6ae3153e72a94dac13
Jacob Oyen <oyenj@central.edu>
2016-01-29 09:09:09 -0600

    Drops the 1 on the phone number. Fixes #15.

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
     <div class="addr one-half column">
       <strong>Central College</strong><br />
       <span class="street-address">812 University St.</span> <span class="locality">Pella</span>, <span class="region">Iowa</span> <span class="postal-code">50219</span> <br />
-      <div class="phone"><a href="tel:877-447-0287">1-877-447-0287</a></div>
+      <div class="phone"><a href="tel:877-447-0287">877-447-0287</a></div>
       <div class="email"><a href="mailto:foreverdutch@central.edu">foreverdutch@central.edu</a></div>
     </div>
     <div class="footerLogo one-half column">
