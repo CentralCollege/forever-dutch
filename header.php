e7e74a2722f517c458c06964791113a5e387c2ab
Jacob Oyen <oyenj@central.edu>
2016-01-29 09:04:18 -0600

    Updated links to social networks and events page. Partially Fixes #11

--- header.php
+++ header.php
@@ -36,11 +36,11 @@
           <a href="/history">History</a>
           <a href="/projects">Projects</a>
           <a href="/category/news/">News</a>
-          <a href="/category/events">Events</a>
+          <a href="/events">Events</a>
           <a href="/donate">Giving Info</a>
           <a href="//www.facebook.com/centralcollege" class="fa fa-facebook"></a>
-          <a href="//www.instagram.com/centralcollege" class="fa fa-twitter"></a>
-          <a href="//www.twitter.com/centralcollege" class="fa fa-instagram"></a>
+          <a href="//www.twitter.com/centralcollege" class="fa fa-twitter"></a>
+          <a href="//www.instagram.com/centralcollege" class="fa fa-instagram"></a>
         </div>
       </nav>
     </header>
