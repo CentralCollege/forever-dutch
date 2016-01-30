9ca8b4322994a667839c41dba9d4017e406dbae8
Jacob Oyen <oyenj@central.edu>
2016-01-30 15:02:54 -0600

    Added mobile formatting. Fixes #10

--- home.php
+++ home.php
@@ -49,12 +49,13 @@
     <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script type="text/javascript">
       function addYouTubeVideo(id,title){
-        var youTubeImage = '<img src="<?php echo bloginfo('template_directory')?>/images/videoThumb.jpg" alt="'+title+'" class="youTubeThumb">';
+        var youTubeImage = '<img src="<?php echo bloginfo('template_directory')?>/images/videoThumb.jpg" alt="'+title+'" class="youTubeThumb" style="width: 100%;">';
         $('.youTubeVideo').append(youTubeImage);
 
         $('.youTubeVideo').click(function(){
           var youTubeEmbed = '<iframe width="99%" height="350" src="//www.youtube.com/embed/'+id+'/?rel=0&showinfo=0&autohide=1&vq=hd720&autoplay=true"></iframe>';
           $('.youTubeVideo').html(youTubeEmbed);
+          _gaq.push(['_trackEvent', 'Forever Dutch Video', 'Play', 'Homepage Video Play']);
         });
         $('.youTubeVideo').hover(function(){
           $('.playButtonImg').attr('src','//img.centralcollege.info/main/playButtonHover.png');
