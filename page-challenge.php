cfa173ac94233a4ef74493cfccb40f46387f7384
Jacob Oyen <oyenj@central.edu>
2016-10-14 07:03:57 -0500

    First attempt at solving #69.

---
+++ page-challenge.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+  <section class="red keystone">
+    <div class="container">
+      <h1>Challenge - Lorem Ipsum</h1>
+      <h2>Lorem ipsum dolor sit amet</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in sagittis nisl. Proin rhoncus vestibulum arcu, a varius nulla fringilla vitae. Maecenas ac lobortis leo. Duis blandit laoreet metus, nec egestas velit mollis in.</p>
+    </div>
+  </section>
+  <section class="give">
+    <div class="container breathe newsEvents">
+      <div class="one-half column">
+        <h2>Yes, I want to take the challenge</h2>
+        <img src="//placekitten.com/400/400" alt="placeholder">
+        <p class="alignCenter"><a href="/give/" class="redButton">Give Now</a></p>
+      </div>
+      <div class="one-half column">
+        <h2>I want to challenge a friend</h2>
+        <img src="//placekitten.com/400/400" alt="placeholder">
+        <p class="alignCenter"><a href="/challenge-a-friend/" class="redButton">Challenge a friend</a></p>
+      </div>
+      <div class="clearBoth"></div>
+    </div>
+  </section>
+<?php get_footer();?>
