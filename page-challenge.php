644c2df0a2b6eb59e19f03ff27a0b3f1bde3e86f
Jordan Bohr <bohrj1@central.edu>
2016-10-24 18:22:21 -0500

    File for /challenge page.

---
+++ page-challenge.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+  <section class="red keystone">
+    <div class="container">
+      <h1>Take the challenge</h1>
+      <h2>This is my time! This my team!</h2>
+      <p>Take the challenge or challenge a friend. Be forever Dutch!</p>
+    </div>
+  </section>
+  <section class="give">
+    <div class="container breathe newsEvents">
+      <div class="one-half column" style="border-right: 3px solid #cb2026">
+        <h2 class="alignCenter">Yes, I want to take the challenge</h2>
+        <p class="alignCenter"><img src="<?php echo bloginfo('template_directory')?>/images/justMe.svg" alt="placeholder"></p>
+        <p class="alignCenter"><a href="/give/" class="redButton">Give Now</a></p>
+      </div>
+      <div class="one-half column">
+        <h2 class="alignCenter">I want to challenge a friend</h2>
+        <p class="alignCenter"><img src="<?php echo bloginfo('template_directory')?>/images/MePlusFriend.svg" alt="placeholder"></p>
+        <p class="alignCenter"><a href="/challenge-a-friend/" class="redButton">Challenge a friend</a></p>
+      </div>
+      <div class="clearBoth"></div>
+    </div>
+  </section>
+<?php get_footer();?>
