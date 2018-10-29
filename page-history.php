e6ee20f77b83daecb098793a5bb57f61314f6e84
bohrj <bohrj@central.edu>
2018-10-29 18:23:25 -0500

    Copy updates

--- page-history.php
+++ page-history.php
@@ -25,11 +25,11 @@
           <div class="statDetails">All-Americans</div>
         </div>
         <div class="three columns">
-          <div class="stat">162</div>
-          <div class="statDetails">Iowa Conference Championships</div>
+          <div class="stat">164</div>
+          <div class="statDetails">Conference Championships</div>
         </div>
         <div class="three columns">
-          <div class="stat">235</div>
+          <div class="stat">245</div>
           <div class="statDetails">All-Americans</div>
         </div>
         <div class="clearBoth"></div>
@@ -38,12 +38,12 @@
     <section class="lightGray dropTop">
       <div class="top container">
         <h2 class="redText"><img src="<?php echo bloginfo('template_directory')?>/images/echelon.svg" class="eschelon">The heart of the A.N. Kuyper Athletics Complex and the proud Dutch athletics tradition: P.H. Kuyper Gymnasium.</h2>
-        <p>P.H. Kuyper Gymnasium is the headquarters for Central’s athletics program. However, more than 45 years after construction, it’s no longer able to match the quality of Central’s athletes and teams.</p>
+        <p>P.H. Kuyper Gymnasium is the headquarters for Central’s athletics program. However, nearly 50 years after construction, it’s no longer able to match the quality of Central’s athletes and teams.</p>
         <p>&nbsp;</p>
       </div>
     </section>
     <section class="comparison top exteriorMockAfter">
-      <img src="<?php echo bloginfo('template_directory')?>/images/comparison.svg" alt="Stat Comparison">
+      <img src="<?php echo bloginfo('template_directory')?>/images/comparison.svg?v=2" alt="Stat Comparison">
     </section>
     <section class="red">
       <div class="breathe container">
@@ -56,16 +56,14 @@
         <div class="clearBoth"></div>
       </div>
     </section>
-    <img src="<?php echo bloginfo('template_directory')?>/images/kuyperHallway.jpg" alt="Schipper Stadium" class="u-max-full-width">
-    <section class="darkGray">
+    <section class="darkGray dropTop">
       <div class="breathe container">
-        <h2 class="redText">Out of Room.</h2>
-        <p><strong>P.H. Kuyper Gymnasium’s aging infrastructure must be replaced.</strong> And a building designed to serve 200 athletes in 1969, now must handle more than 700, turning hallways into athletic training treatment areas. Facilities are in use from 5 a.m.-11 p.m.</p>
-        <p>It’s often been said that the athletics department is a college’s front porch. Student-athletes comprise a significant part of Central’s enrollment and many of the college’s competitors are making major athletics facilities upgrades.</p>
-        <p><strong>This is the time not just for a renovation, but a transformation that will accentuate Central’s position as an intercollegiate leader, as well as a model for whole-person development.</strong></p>
+        <h2 class="redText">Unfinished</h2>
+        <p><strong>Work remains.</strong> While portions of P.H. Kuyper Gymnasium’s aging infrastructure have been replaced, existing locker rooms, meeting/recruiting spaces, the athletic training room and offices are untouched an in pressing need of upgrade.</p>
+        <p><strong>This is the time for not just a renovation, but a transformation that will accentuate Central’s position as an intercollegiate athletics leader, as well as a model for whole-person development.</strong></p>
       </div>
     </section>
-<img src="<?php echo bloginfo('template_directory')?>/images/stadium.jpg" alt="Schipper Stadium" class="u-max-full-width">
+<img src="<?php echo bloginfo('template_directory')?>/images/displayCorridor.jpg" alt="Display Corridor in P.H. Kuyper Gymnasium" class="u-max-full-width">
     <section class="quoteSpace">
       <div class="container">
         <blockquote>Football made me set standards for myself as an athlete, as well as a student—to do everything with a passion and a purpose, and to do it to my full capability. There is no doubt in my mind that playing  football at Central College changed my life, and it changed it for the better." <cite>Travis Hook '13</cite></blockquote>
