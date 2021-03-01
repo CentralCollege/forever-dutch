a86755b2b2824a34af50ccdb6e1e271421ac4f26
bohrj <bohrj@central.edu>
2021-03-01 17:38:03 -0600

    Update page-projects.php
    - Updates content and photos for Projects page

--- page-projects.php
+++ page-projects.php
@@ -3,10 +3,10 @@
       <div class="container">
         <h1>A Transformation</h1>
         <h2>Championship facilities for an elite program</h2>
-        <p>The transformation of the A.N. Kuyper Athletics Complex has begun and we need your help to see it through. Forever Dutch began with outdoor field enhancements followed by the expansion of P.H. Kuyper Gymnasium. Next is the building’s renovation and possible additional construction.</p>
+        <p>The Forever Dutch<sup>®</sup> initiative is part of an overall transformation of the A.N. Kuyper Athletics Complex. It began with outdoor field enhancements and is now followed with the expansion and renovation of P.H. Kuyper Gymnasium.</p>
       </div>
     </section>
-    <section class="phase">
+    <!-- <section class="phase">
       <div class="container breathe">
         <div class="one-half column">
           <h2>P.H. Kuyper Gymnasium Renovation: <span class="grayText">Next</span></h2>
@@ -15,7 +15,7 @@
     </section>
     <section class="photo">
       <div class="container">
-      <!--- First Row !--->
+
           <div class="ten column">
             <div class="imageGroup">
               <a href="<?php echo bloginfo('template_directory')?>/images/lockerRoom-large.jpg?v=2" data-lightbox="renders" data-title="Locker Rooms"><img src="<?php echo bloginfo('template_directory')?>/images/lockerRoom.jpg?v=2" alt="Locker Rooms"></a>
@@ -23,7 +23,7 @@
             </div>
           </div>
           <div class="clearBoth"></div>
-      <!--- Second Row !--->
+
         <div class="ten column">
           <div class="imageGroup">
             <a href="<?php echo bloginfo('template_directory')?>/images/trainingRoom-large.jpg?v=2" data-lightbox="renders" data-title="Athletic Training Room"><img src="<?php echo bloginfo('template_directory')?>/images/trainingRoom.jpg?v=2" alt="Athletic Training Room"></a>
@@ -31,7 +31,7 @@
           </div>
         </div>
         <div class="clearBoth"></div>
-      <!--- Third Row !--->
+
         <div class="ten column">
           <div class="imageGroup">
             <a href="<?php echo bloginfo('template_directory')?>/images/welcomeCenter-large.jpg?v=2" data-lightbox="renders" data-title="Large Office"><img src="<?php echo bloginfo('template_directory')?>/images/welcomeCenter.jpg?v=3" alt="Welcome Center"></a>
@@ -39,7 +39,7 @@
           </div>
         </div>
         <div class="clearBoth">
-      <!--- Fourth Row !--->
+
         <div class="hard-half column">
           <div class="imageGroup">
             <a href="<?php echo bloginfo('template_directory')?>/images/largeOffice-large.jpg?v=2" data-lightbox="renders" data-title="Large Office"><img src="<?php echo bloginfo('template_directory')?>/images/largeOffice.jpg?v=2" alt="Large Office"></a>
@@ -55,17 +55,7 @@
         <div class="clearBoth">
 
       </div>
-    </section>
-
-    <section class="section darkGray">
-      <div class="container breathe">
-        <h2>P.H. Kuyper Gymnasium Expansion Walk-Through</h2> &nbsp;
-        <div class="twelve columns alignCenter">
-          <div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://www.youtube.com/embed/XRomzenp1fY?rel=0&amp;showinfo=0" width="100%" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>
-        </div>
-        <div class="clearBoth"></div>
-      </div>
-    </section>
+    </section> -->
 
     <section class="quoteSpace breathe">
       <div class="container">
@@ -77,11 +67,10 @@
     </section>
 
     <section class="phase">
-      <div class="container breathe">
-          <h2>P.H. Kuyper Gymnasium Expansion: <span class="grayText">Complete</span></h2>
+      <div class="container">
+          <h2>P.H. Kuyper Gymnasium Expansion/Renovation: <span class="grayText">Complete</span></h2>
           <p class="redText"><strong>Open April 20, 2017</strong></p>
-          <h4 class="redText">P.H. Kuyper Gymnasium Expansion</h4>
-          <ul class="list-group">
+          <!-- <ul class="list-group">
             <li class="list-group-item"><i class="fa fa-check-square fa-fw"></i>New P.H. Kuyper Gymnasium Entrance</li>
             <li class="list-group-item"><i class="fa fa-check-square fa-fw"></i>New Athletics Hall of Honor</li>
             <li class="list-group-item"><i class="fa fa-check-square fa-fw"></i>New Lower-Level Lobby</li>
@@ -90,72 +79,98 @@
             <li class="list-group-item"><i class="fa fa-check-square fa-fw"></i>New Varsity Locker Room</li>
             <li class="list-group-item"><i class="fa fa-check-square fa-fw"></i>New Wrestling Room</li>
             <li class="list-group-item"><i class="fa fa-check-square fa-fw"></i>Major Infrastructure Improvements</li>
-          </ul>        <div class="one-half column breathe">
+          </ul> -->
         <div class="clearBoth"></div>
     </section>
     <section class="photo">
       <!--- First Row !--->
       <div class="container">
-      <div class="ten column">
+        <div class="ten column">
+          <div class="imageGroup">
+            <a href="<?php echo bloginfo('template_directory')?>/images/southEntry-large.jpg?v=2" data-lightbox="renders" data-title="Gym Entry"><img src="<?php echo bloginfo('template_directory')?>/images/southEntry.jpg?v=2" alt="Gym Entry"></a>
+            <div class="captionLeft">Entrance</div>
+          </div>
+        </div>
+        <div class="clearBoth"></div>
+        <!--- Second Row !--->
+          <div class="hard-half column">
+            <div class="imageGroup">
+              <a href="<?php echo bloginfo('template_directory')?>/images/wrestlingRoom-large.jpg" data-lightbox="renders" data-title="Wrestling Room"><img src="<?php echo bloginfo('template_directory')?>/images/wrestlingRoom.jpg" alt="Wrestling Room"></a>
+              <div class="captionLeft">Wrestling Room</div>
+            </div>
+          </div>
+          <div class="hard-half column">
+            <div class="imageGroup">
+              <a href="<?php echo bloginfo('template_directory')?>/images/upperAtrium-large.jpg" data-lightbox="renders" data-title="Upper-Level Atrium"><img src="<?php echo bloginfo('template_directory')?>/images/upperAtrium.jpg" alt="Upper-Level Atrium"></a>
+              <div class="captionRight">Upper-Level Atrium</div>
+            </div>
+          </div>
+          <div class="clearBoth"></div>
+        <!--- Third Row !--->
+
+          <div class="hard-half column">
+            <div class="imageGroup">
+              <a href="<?php echo bloginfo('template_directory')?>/images/lowerAtrium-large.jpg?v=2" data-lightbox="renders" data-title="Lower-Level Atrium"><img src="<?php echo bloginfo('template_directory')?>/images/lowerAtrium.jpg?v=2" alt="Lower-Level Atrium"></a>
+              <div class="captionLeft">Lower-Level Atrium</div>
+            </div>
+          </div>
+          <div class="hard-half column">
+            <div class="imageGroup">
+              <a href="<?php echo bloginfo('template_directory')?>/images/allAmericanHall-large.jpg?v=2" data-lightbox="renders" data-title="All-America Corridor"><img src="<?php echo bloginfo('template_directory')?>/images/allAmericanHall.jpg?v=2" alt="All-America Corridor"></a>
+              <div class="captionRight">Display Corridor</div>
+            </div>
+          </div>
+        <div class="clearBoth"></div>
+        <!--- Fourth Row !--->
+        <div class="ten column">
+          <div class="imageGroup">
+            <a href="<?php echo bloginfo('template_directory')?>/images/lockerRoomNew-large.jpg" data-lightbox="renders" data-title="Varsity Locker Room"><img src="<?php echo bloginfo('template_directory')?>/images/lockerRoomNew.jpg" alt="Varsity Locker Room"></a>
+            <div class="captionLeft">Varsity Locker Room</div>
+          </div>
+        </div>
+        <div class="clearBoth"></div>
+        <!--- Fifth Row !--->
+        <div class="ten column">
+          <div class="imageGroup">
+            <a href="<?php echo bloginfo('template_directory')?>/images/southwestEntry-large.jpg?v=2" data-lightbox="renders" data-title="Southwest Entry"><img src="<?php echo bloginfo('template_directory')?>/images/southwestEntry.jpg?v=2" alt="Southwest Entry"></a>
+            <div class="captionLeft">Southwest Entry</div>
+          </div>
+        </div>
+        <div class="clearBoth"></div>
+        <!--- Sixth Row !--->
+        <div class="hard-half column">
+          <div class="imageGroup">
+            <a href="https://forever.central.edu/wp-content/uploads/2021/03/Athletic-training-room_WEB-large.jpg" data-lightbox="renders" data-title="Athletic Training Room"><img src="https://forever.central.edu/wp-content/uploads/2021/02/Athletic-training-room_WEB.jpg" alt="Athletic Training Room"></a>
+            <div class="captionLeft">Athletic Training Room</div>
+          </div>
+        </div>
+        <div class="hard-half column">
+          <div class="imageGroup">
+            <a href="https://forever.central.edu/wp-content/uploads/2021/03/Upper-level-hallway_WEB-large.jpg" data-lightbox="renders" data-title="Upper-Level Hallway"><img src="https://forever.central.edu/wp-content/uploads/2021/02/Upper-level-hallway_WEB.jpg" alt="Upper-Level Hallway"></a>
+            <div class="captionRight">Upper-Level Hallway</div>
+          </div>
+        </div>
+      <div class="clearBoth"></div>
+      <!--- Seventh Row !--->
+      <div class="hard-half column">
         <div class="imageGroup">
-          <a href="<?php echo bloginfo('template_directory')?>/images/southEntry-large.jpg?v=2" data-lightbox="renders" data-title="Gym Entry"><img src="<?php echo bloginfo('template_directory')?>/images/southEntry.jpg?v=2" alt="Gym Entry"></a>
-          <div class="captionLeft">Entrance</div>
+          <a href="https://forever.central.edu/wp-content/uploads/2021/03/Former-reception-area-demolition_WEB-large.jpg" data-lightbox="renders" data-title="Former Reception Area"><img src="https://forever.central.edu/wp-content/uploads/2021/02/Former-reception-area-demolition_WEB.jpg" alt="Former Reception Area"></a>
+          <div class="captionLeft">Former Reception Area</div>
         </div>
       </div>
-      <div class="clearBoth"></div>
-      <!--- Second Row !--->
-        <div class="hard-half column">
-          <div class="imageGroup">
-            <a href="<?php echo bloginfo('template_directory')?>/images/wrestlingRoom-large.jpg" data-lightbox="renders" data-title="Wrestling Room"><img src="<?php echo bloginfo('template_directory')?>/images/wrestlingRoom.jpg" alt="Wrestling Room"></a>
-            <div class="captionLeft">Wrestling Room</div>
-          </div>
+      <div class="hard-half column">
+        <div class="imageGroup">
+          <a href="https://forever.central.edu/wp-content/uploads/2021/03/New-office-area_WEB-large.jpg" data-lightbox="renders" data-title="New Office Area"><img src="https://forever.central.edu/wp-content/uploads/2021/02/New-office-area_WEB.jpg" alt="New Office Area"></a>
+          <div class="captionRight">New Office Area</div>
         </div>
-        <div class="hard-half column">
-          <div class="imageGroup">
-            <a href="<?php echo bloginfo('template_directory')?>/images/upperAtrium-large.jpg" data-lightbox="renders" data-title="Upper-Level Atrium"><img src="<?php echo bloginfo('template_directory')?>/images/upperAtrium.jpg" alt="Upper-Level Atrium"></a>
-            <div class="captionRight">Upper-Level Atrium</div>
-          </div>
-        </div>
-        <div class="clearBoth">
-      <!--- Third Row !--->
-
-        <div class="hard-half column">
-          <div class="imageGroup">
-            <a href="<?php echo bloginfo('template_directory')?>/images/lowerAtrium-large.jpg?v=2" data-lightbox="renders" data-title="Lower-Level Atrium"><img src="<?php echo bloginfo('template_directory')?>/images/lowerAtrium.jpg?v=2" alt="Lower-Level Atrium"></a>
-            <div class="captionLeft">Lower-Level Atrium</div>
-          </div>
-        </div>
-        <div class="hard-half column">
-          <div class="imageGroup">
-            <a href="<?php echo bloginfo('template_directory')?>/images/allAmericanHall-large.jpg?v=2" data-lightbox="renders" data-title="All-America Corridor"><img src="<?php echo bloginfo('template_directory')?>/images/allAmericanHall.jpg?v=2" alt="All-America Corridor"></a>
-            <div class="captionRight">Display Corridor</div>
-          </div>
-        </div>
-        <div class="clearBoth">
-
-
-          <div class="ten column">
-            <div class="imageGroup">
-              <a href="<?php echo bloginfo('template_directory')?>/images/lockerRoomNew-large.jpg" data-lightbox="renders" data-title="Varsity Locker Room"><img src="<?php echo bloginfo('template_directory')?>/images/lockerRoomNew.jpg" alt="Varsity Locker Room"></a>
-              <div class="captionLeft">Varsity Locker Room</div>
-            </div>
-          </div>
-          <div class="clearBoth"></div>
-
-          <div class="ten column">
-            <div class="imageGroup">
-              <a href="<?php echo bloginfo('template_directory')?>/images/southwestEntry-large.jpg?v=2" data-lightbox="renders" data-title="Southwest Entry"><img src="<?php echo bloginfo('template_directory')?>/images/southwestEntry.jpg?v=2" alt="Southwest Entry"></a>
-              <div class="captionLeft">Southwest Entry</div>
-            </div>
-          </div>
-          <div class="clearBoth"></div>
-
+      </div>
+    <div class="clearBoth"></div>
       </div>
     </section>
 
-    <section class="phase">
-      <div class="container breathe">
-        <h2>Outdoor Field Upgrades: <span class="grayText">Complete</span></h2>
+    <section class="phase breathe">
+      <div class="container">
+        <h2>Outdoor Field Upgrades<!-- : <span class="grayText">Complete</span> --></h2>
         <ul class="list-group">
           <li class="list-group-item"><i class="fa fa-check-square fa-fw"></i>New Baseball Dugouts, Stadium Seating</li>
           <li class="list-group-item"><i class="fa fa-check-square fa-fw"></i>New Softball Stadium Seating</li>
@@ -214,7 +229,15 @@
       </div>
     </section>
 
-
+    <section class="section darkGray">
+      <div class="container breathe">
+        <h2>P.H. Kuyper Gymnasium Expansion Walk-Through</h2> &nbsp;
+        <div class="twelve columns alignCenter">
+          <div style="position:relative;height:0;padding-bottom:56.25%"><iframe src="https://www.youtube.com/embed/XRomzenp1fY?rel=0&amp;showinfo=0" width="100%" height="360" frameborder="0" style="position:absolute;width:100%;height:100%;left:0" allowfullscreen></iframe></div>
+        </div>
+        <div class="clearBoth"></div>
+      </div>
+    </section>
 
     <section class="red steeringCommittee breathe">
       <div class="container">
@@ -245,7 +268,7 @@
         <div class="clearBoth"></div>
       </div>
     </section>
-    <section class="darkGray breathe">
+    <!-- <section class="darkGray breathe">
       <div class="container">
         <div class="four columns">
           <img src="<?php echo bloginfo('template_directory')?>/images/jersey.svg" alt="Fundraising Total" class="u-max-full-width">
@@ -257,7 +280,7 @@
         </div>
         <div class="clearBoth"></div>
       </div>
-    </section>
+    </section> -->
     <section class="quoteSpace">
       <div class="container">
         <blockquote>I think we have something special happening here. This is our time to add something extra to this rich tradition.”
