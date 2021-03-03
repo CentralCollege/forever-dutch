ec1b3dc5ee315722fb1bff7f3768aa0cc966179d
Jordan Bohr <bohrj@central.edu>
2021-03-03 15:15:02 -0600

    Update page-donate.php
    - Updates content for March 2021
    - Finalizes content based on last feedback

--- page-donate.php
+++ page-donate.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
     <section class="red keystone">
       <div class="container">
-        <h1>Your Time</h1>
-        <h2>A financial investment. An enduring legacy.</h2>
-        <p>Those who have come before us helped create the Central legacy. Now, at this defining moment, it falls to this generation to carry it forward. The need is great. The time is now. Your investment helps reaffirm our pledge to further the intellectual, physical and spiritual development of future Central students.</p>
-        <p><a href="/give/" class="whiteButton">Give Now</a></p>
+        <h1>AN ENDURING LEGACY</h1>
+        <!-- <h2>A financial investment. An enduring legacy.</h2> -->
+        <p style="font-size:larger;">A record 2,712 Central alumni and friends provided support for Forever Dutch<sup>&reg;</sup>, the most donors ever for a college project.</p>
+        <!-- <p><a href="/give/" class="whiteButton">Give Now</a></p> -->
       </div>
     </section>
 
-    <section class="contactUs lightGray breathe">
+    <!-- <section class="contactUs lightGray breathe">
       <div class="container">
         <p>Naming opportunities are available with gifts of $25,000 or more.</p>
         <h2>Contact Us</h2>
@@ -26,8 +26,8 @@
         </div>
         <div class="clearBoth"></div>
       </div>
-    </section>
-    <section class="fundraisingTotal breathe">
+    </section> -->
+    <!-- <section class="fundraisingTotal breathe">
       <div class="container">
         <div class="eight columns offset-by-two">
           <h2>Forever Dutch Endowment Fund</h2>
@@ -36,11 +36,91 @@
         </div>
         <div class="clearBoth"></div>
       </div>
+    </section> -->
+		<section class="quoteSpace">
+      <div class="top container breathe">
+        <blockquote>
+          We are so grateful to all the alumni and friends of the college who have given thoughtfully and generously to help us finish this project for our students and coaches. Time and time again, members of the Central family have demonstrated their unyielding support. We send our deepest thanks for their confidence in Central and belief in our mission. Through their generosity, we are able to optimize the Central student-athlete experience now and well into the future.
+          <cite><strong>Sunny Gonzales Eighmy ’99 </strong><br>Vice President for Advancement </cite>
+        </blockquote>
+      </div>
     </section>
-    <section class="coachSkipQuote">
+		<section class="photo">
+			<!--- First Row !--->
+			<div class="container">
+				<div class="twelve column">
+					<div class="imageGroup">
+						<img src="https://forever.central.edu/wp-content/uploads/2021/03/Ashford-donor-page_WEB.jpg" alt="Action shot from a basketball game in the newly-renovated P.H. Kuyper Gymnasium.">
+					</div>
+				</div>
+				<div class="clearBoth"></div>
+			</div>
+		</section>
+
+		<section class="donorsList">
+      <div class="top container breathe">
+				<h1>Supporters</h1>
+				<h2 class="redText" style="font-size:xx-large">Lead Gift &mdash; $3.4 Million</h2>
+				<p>
+					<ul class="redText">
+						<li><a href="/history/the-kuyper-legacy/">M. Joan Farver</a></li>
+					</ul>
+				</p>
+				&nbsp;
+				<h2>Hall of Fame &mdash; $1 million-plus</h2>
+				<p>
+					<ul>
+						<li>Curt &amp; Mary Blythe</li>
+					</ul>
+				</p>
+				<h2>National Champions &mdash; $250,000 to $1 million</h2>
+				<p>
+					<ul>
+						<li>Mark &amp; Kay De Cook</li>
+						<li>Peter H. &amp; E. Lucille Gaass Kuyper Foundation</li>
+						<li>Tom &amp; Debbie Koos</li>
+						<li>Larry &amp; Cathy Pacha</li>
+						<li>Stan &amp; Gayle Poortinga</li>
+						<li>Precision, Inc.</li>
+						<li>Harry Smith &amp; Andrea Joyce</li>
+						<li>Dave &amp; Ardie Sutphen</li>
+						<li>Don &amp; Diana Vogel</li>
+					</ul>
+				</p>
+				<h2>All-Americans &mdash; $100,000-$250,000</h2>
+				<p>
+					<ul>
+						<li>Dave &amp; Kristi Balk</li>
+						<li>Dody Boat</li>
+						<li>Pete &amp; Rebecca Cartwright</li>
+						<li>The Central Club</li>
+						<li>Vern &amp; Diane Den Herder</li>
+						<li>Don &amp; Jean De Waard</li>
+						<li>Chip Griffith</li>
+						<li>Doug &amp; Nyla Heerema</li>
+						<li>Sandy Heerema</li>
+						<li>Pete &amp; Jackie Kuyper</li>
+						<li>Marion County Bank</li>
+						<li>Mike &amp; Linda Orr</li>
+						<li>Rick &amp; Sarah Ryan</li>
+						<li>John &amp; Betty Sikkink</li>
+						<li>Steve &amp; Penny Sikkink</li>
+						<li>Dave &amp; Kathy Smith</li>
+						<li>Clay Thompson &amp; Amy Britt</li>
+						<li>Dave &amp; Linda Wesselink</li>
+						<li>Curt &amp; Mary Zuber</li>
+					</ul>
+				</p>
+      </div>
+    </section>
+    <section class="coachSkipQuote dropTop">
       <div class="top container">
-        <p><a href="/give/" class="redButton pullRight">Give Now</a></p>
-        <blockquote>If I can light a fire under one kid, then it's all worth it.<cite>Coach Ron Schipper</cite></blockquote>
+				<div class="twelve columns">
+					<!-- <p><a href="/give/" class="redButton pullRight">Give Now</a></p> -->
+	        <blockquote>If I can light a fire under one kid, then it's all worth it.
+						<cite>Coach Ron Schipper</cite>
+					</blockquote>
+				</div>
       </div>
     </section>
     <?php get_footer();?>
